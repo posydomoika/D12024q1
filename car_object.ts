8b4b34cc37735b37d7fbd0a5a8245df1fee0a0d1
FIRST_NAME LAST_NAME <FIRST_NAME LAST_NAME>
2024-10-10 18:33:09 +0300

    add: object car

---
+++ car_object.ts
@@ -0,0 +1,9 @@
+const patriot ={
+    salon: 'темный',
+    securitySystems: 'Антипробуксовочная',
+    exterior:'Легкосплавные диски',
+    climate:'Климат-контроль',
+    Multimedia:'Штатная навигация',
+}
+patriot.salon='светлый'
+console.log(patriot)
